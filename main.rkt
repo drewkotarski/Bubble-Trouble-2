7e675707c6662d9fa7b86664ec4ac0ef38b42454
mdanino94 <michael_danino@student.uml.edu>
2017-04-17 20:22:21 -0400

    added basic HUD
    can be built on and refined before the coming presentation

--- main.rkt
+++ main.rkt
@@ -6,6 +6,25 @@
 (require "objects.rkt")
 
 
+(define num-lives 3)
+
+(define level #\1)
+
+(define (draw-lives n)
+  (if (> n 0)
+      (beside (circle 8 "solid" "black") (draw-lives (- n 1))) empty-image))
+
+(define (draw-HUD)
+   (underlay/xy
+    (underlay/xy (rectangle 1100 30 "solid" "red")
+                0
+                5
+                (draw-lives num-lives))
+    500
+    5
+    (text (string #\L #\E #\V #\E #\L #\: #\ level) 20 "black")))
+    
+;(text (string #\L #\I #\V #\E #\S #\: #\ (integer->char num-lives)) 5 "black")))
 (define arrowSound (rs-read "arrow.wav"));read in the arrow sound to be played upon shooting
 ;(play arrowSound)
 ; initialize the main player and the "hook" used (eventually) to pop bubbles
@@ -20,12 +39,11 @@
   retval)
           
 (define my-hook-sprite (bitmap "arrow.png"))
-(define invis_obj (circle 0 "outline" "white"))
 
 (define background (bitmap "background.jpg"))
 
 (define (hook-sprite)
-  (if (my-hook 'is-shooting?) my-hook-sprite invis_obj))
+  (if (my-hook 'is-shooting?) my-hook-sprite empty-image))
 
 ;handle key events
 (define (change w a-key)
@@ -35,27 +53,32 @@
         [else (p1 'face-up)]))
 
 (define (world-obj)
-  (underlay/xy
-  (underlay/xy
+  (underlay/xy 
    (underlay/xy
-    (underlay/xy background
-                 (p1 'position) ; x val of p1
-                 600 ; y val of p1
-                 (p1-sprite))
-    (bubble1 'x)
-    (bubble1 'y)
-    (bubble1 'draw))
-   (+ 10 (my-hook 'x))
-   (+ (my-hook 'y) 7)
-   (draw-chain-2))
-  (my-hook 'x)
-  (my-hook 'y)
-  (hook-sprite)))
+    (underlay/xy
+     (underlay/xy
+      (underlay/xy background
+                   (p1 'position) ; x val of p1
+                   600 ; y val of p1
+                   (p1-sprite))
+      (bubble1 'x)
+      (bubble1 'y)
+      (bubble1 'draw))
+     (+ 10 (my-hook 'x))
+     (+ (my-hook 'y) 7)
+     (draw-chain-2))
+    (my-hook 'x)
+    (my-hook 'y)
+    (hook-sprite))
+   0
+   670
+   (draw-HUD))
+  )
 
 (define (draw-chain-2)
   (if (my-hook 'is-shooting?)
   (overlay (rectangle 4 (- 600 (my-hook 'y) 8) "solid" "brown") (rectangle 5 (- 600 (my-hook 'y) 7) "solid" "gray"))
-  invis_obj))
+  empty-image))
 
 (define (draw-chain y)
   (if (and (< y 600) (my-hook 'is-shooting?))
@@ -63,7 +86,7 @@
                    (my-hook 'x)
                    y
                    (draw-chain (+ y 7)))
-      invis_obj))
+      empty-image))
 
 (define (update-screen x)
   (world-obj))
